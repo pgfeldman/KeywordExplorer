36141bb7c44e9ab5ac6c70ce3ed37f491c4d8d36
Phil <philip.feldman@asrcfederal.com>
2023-01-17 18:11:59 -0500

    Added date and site-specific search to google_search.py

--- keyword_explorer/utils/google_search.py
+++ keyword_explorer/utils/google_search.py
@@ -15,20 +15,42 @@ engines = {
     'news': '017379340413921634422:wvavj4i3sbu'
 }
 
+class GoogleCSEInfo:
+    title:str
+    searchTerms:str
+    totalResults:int
+    count:int
+    d:Dict
+
+    def __init__(self, d:Dict):
+        self.d = d
+        self.title = d['title']
+        self.searchTerms = d['searchTerms']
+        self.totalResults = d['totalResults']
+        self.count = d['count']
+
+    def to_string(self) -> str:
+        #return "{}:\n\tlink = {}\n\tsnippet = {}".format(self.title, self.display_link, self.snippet)
+        return "{}".format(self.d)
+
+
 class GoogleCSEResult:
     title:str
     link:str
     display_link:str
     snippet:str
+    d:Dict
 
     def __init__(self, d:Dict):
+        self.d = d
         self.title = d['title']
         self.link = d['link']
         self.display_link = d['displayLink']
         self.snippet = d['snippet']
 
     def to_string(self) -> str:
-        return "{}:\n\tlink = {}\n\tsnippet = {}".format(self.title, self.display_link, self.snippet)
+        #return "{}:\n\tlink = {}\n\tsnippet = {}".format(self.title, self.display_link, self.snippet)
+        return "{}".format(self.d)
 
     def to_html(self) ->str:
         s = '<p>{}</p>\n<ul>\n<li>link = <a href="{}">{}</a></li>\n<li>snippet = {}</li>\n</ul>'.format(
@@ -37,19 +59,28 @@ class GoogleCSEResult:
         return s
 
 
-def get_search_results_list(query:str, engine:str, key:str) -> List:
+def get_search_results_list(query:str, engine:str, key:str, arg_dict:Dict = None) -> (List, List):
+    info_list = []
     results_list = []
     safe_query = urllib.parse.quote(query)
     #s = "https://www.googleapis.com/customsearch/v1?key={}&cx={}&q=list%20of%20ethnic%20foods".format(key, engine)
     s = "https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}".format(key, engine, safe_query)
+    if arg_dict != None:
+        for k, v in arg_dict.items():
+            s = "{}&{}={}".format(s, k, v)
     print(s)
     r = requests.get(s)
     # print(r)
     if r.status_code == 200:
-        for item in r.json()['items']:
+        j = r.json()
+        qd = j['queries']
+        for item in qd['request']:
+            g = GoogleCSEInfo(item)
+            info_list.append(g)
+        for item in j['items']:
             g = GoogleCSEResult(item)
             results_list.append(g)
-    return results_list
+    return (results_list, info_list)
 
 def key_exists(key:str = "GOOGLE_CSE_KEY") -> bool:
     val = os.environ.get(key)
@@ -59,12 +90,18 @@ def key_exists(key:str = "GOOGLE_CSE_KEY") -> bool:
 
 def main():
 
-    engine = engines['all.com']
+    engine = engines['com-org-edu']
     key = os.environ.get("GOOGLE_CSE_KEY")
-    l = get_search_results_list("slang for COVID-19", engine, key)
-    g:GoogleCSEResult
-    for g in l:
-        print("\n{}".format(g.to_html()))
+    arg_dict = {"siteSearch":"'twitter.com'", "sort":"date:r:20140815:20140931"}
+    lr, li = get_search_results_list("Feldman", engine, key, arg_dict)
+    gi:GoogleCSEInfo
+    for gi in li:
+        print("info: {}".format(gi.to_string()))
+
+    gr:GoogleCSEResult
+    for gr in lr:
+        #print("\n{}".format(g.to_html()))
+        print("result: {}".format(gr.to_string()))
 
 if __name__ == "__main__":
     main()
