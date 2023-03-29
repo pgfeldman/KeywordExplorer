dae67534b435f84b00224ec270f2da79912b9756
pgfeldman <PGF_second_2>
2023-03-29 14:30:35 -0400

    Added UI bits to support recursive list generation

--- keyword_explorer/Apps/NarrativeExplorer2.py
+++ keyword_explorer/Apps/NarrativeExplorer2.py
@@ -104,7 +104,7 @@ class NarrativeExplorer2(AppBase):
 
         lf = tk.LabelFrame(self, text="Params")
         lf.grid(row=row, column=2, sticky="nsew", padx=5, pady=2)
-        self.build_params(lf, int(text_width/3), int(label_width/2))
+        self.build_params(lf, int(text_width/3), int(label_width*.75))
 
         return row + 1
 
@@ -172,7 +172,7 @@ class NarrativeExplorer2(AppBase):
         self.clusters_field = DataField(lf, row, 'Clusters:', text_width, label_width=label_width)
         row = self.clusters_field.get_next_row()
         self.style_list = ListField(lf, row, "Style", width=text_width, label_width=label_width, static_list=True)
-        self.style_list.set_text(text='Story, List')
+        self.style_list.set_text(text='Story, List, Sequence')
         self.style_list.set_callback(self.set_style_callback)
         ToolTip(self.style_list.tk_list, "Creates a narrative or recursive context for generation - Not implemented")
         row = self.style_list.get_next_row()
@@ -250,6 +250,12 @@ class NarrativeExplorer2(AppBase):
     def set_style_callback(self, event:tk.Event = None):
         style_str = self.style_list.get_selected()
         self.style_list.set_label("Style\n({})".format(style_str))
+        if style_str == "Story":
+            print("Set story regex")
+        elif style_str == "List":
+            print("Set list regex")
+        elif style_str == "Sequence":
+            print("Set Sequence regex")
 
     def set_recurse_depth_callback(self, event:tk.Event = None):
         depth_str = self.recurse_depth_list.get_selected()
