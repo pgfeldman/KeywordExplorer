739531889c8d6d04c59ea6e597c56f35b09351f9
Phil <phil@philfeldman.com>
2023-06-03 17:31:00 -0400

    Adding file management and moar regex

--- keyword_explorer/experiments/meta_json_urltest.py
+++ keyword_explorer/experiments/meta_json_urltest.py
@@ -2,24 +2,27 @@ import re
 import requests
 import json
 import os
-from typing import Dict
+import tkinter as tk
+import tkinter.filedialog
+from typing import Dict, List
 
 
 def count_valid_urls(text):
     # Find all URLs using regular expressions
-    urls = re.findall(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', text)
+    urls = re.findall(r'(https?://\S+(?<![:?#=]))+', text)
 
     # Test each URL and count valid and invalid ones
     valid_count = 0
     invalid_count = 0
     for url in urls:
         try:
-            response = requests.get(url)
+            print("testing url {}".format(url))
+            response = requests.get(url, timeout=2)
             if response.status_code == 200:
                 valid_count += 1
             else:
                 invalid_count += 1
-        except (requests.exceptions.InvalidURL, requests.exceptions.ConnectionError):
+        except (requests.exceptions.InvalidURL, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
             invalid_count += 1
 
     return valid_count, invalid_count
@@ -39,24 +42,63 @@ def read_json_files(directory_path:str) -> Dict:
     return all_dicts
 
 def write_json_files(directory_path, all_dicts:Dict):
-    for key, val in all_dicts.items():
-        filename = key.replace(".json", "_2.json")
-        print("saving {}/{}".format(directory_path, filename))
+    for key, d in all_dicts.items():
+        filename = "url_{}".format(key)
+        fullpath = "{}/{}".format(directory_path, filename)
+        print("saving {}".format(fullpath))
+        with open(fullpath, mode="w", encoding="utf-8") as f:
+            json.dump(d, f, indent=4)
+
+def find_patterns(input_string) -> List:
+    # pattern = r"\(source \d+\)\."
+    pattern = r'\(source (\d+)\)'
+    matches = re.findall(pattern, input_string)
+    return [int(match) for match in matches]
 
 
-def evaluate_data(all_dict:Dict):
+def evaluate_data(all_dicts:Dict):
     print("evaluate_data")
     # go through all the dicts and:
-    # 1) Get the index list from the context and explicitly compare those against the numbers in the "context_response" strings
-    #   Add a "matched sources" column
-    # 2) Get the good URLs and bad URLs for no_context and context responses (4 columns)
-    # 3) Add the new data to the dicts
-    # 4) Add the bad counts to the summary
-    # 5) Write out new json files
+    for name, d in all_dicts.items():
+        print("key = {}".format(name))
+        # 1) Get the index list from the context and explicitly compare those against the numbers in the "context_response" strings
+        context = d['context']
+        source_list = find_patterns(context)
+        print("\t{}".format(source_list))
+        for ed in d['experiments']:
+            context_response = ed['context_response']
+            match_list = []
+            for i in source_list:
+                if str(i) in context_response:
+                    match_list.append(i)
+            ed['index_list'] = match_list
+            print("\ted['index_list'] = {}".format(ed['index_list']))
+            good_urls, bad_urls = count_valid_urls(context_response)
+            ed['ctx_good_urls'] = good_urls
+            ed['ctx_bad_urls'] = bad_urls
+            print("\ted['ctx_good_urls'] = {}, ed['ctx_good_urls'] = {}".format(ed['ctx_good_urls'], ed['ctx_good_urls']))
+            no_context_response = ed['no_context_response']
+            good_urls, bad_urls = count_valid_urls(no_context_response)
+            ed['no_ctx_good_urls'] = good_urls
+            ed['no_ctx_bad_urls'] = bad_urls
+            print("\ted['no_ctx_good_urls'] = {}, ed['no_ctx_bad_urls'] = {}".format(ed['no_ctx_good_urls'], ed['no_ctx_bad_urls']))
+            ed['no_ctx_good_sources'] = 0
+            ed['no_ctx_bad_sources'] = 0
 
 
 def main():
     print("meta_json_urltest")
+    root = tk.Tk()
+    root.withdraw()
+
+    # open the file dialog and get the selected directory
+    path = tkinter.filedialog.askdirectory()
+    all_dicts = read_json_files(path)
+
+    evaluate_data(all_dicts)
+
+    write_json_files(path, all_dicts)
+
 
 if __name__ == "__main__":
     main()
